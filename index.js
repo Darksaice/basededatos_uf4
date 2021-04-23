c8f231bd5e3aae4e95c16918cfc3f5fcca97e1a1
Darksaice <mitjansperezsara@gmail.com>
2021-04-23 11:27:17 +0200

    poniendo chats

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const mongo = require("mongodb").MongoClient;
 
 let server_url = "mongodb://localhost:27017";
 
+let chats_db;
+
 mongo.connect(server_url, (err, server) => {
 	if (err){
 		console.log("Error en la conexión a MongoDB");
@@ -15,24 +17,51 @@ mongo.connect(server_url, (err, server) => {
 
 	console.log("Dentro de MongoDB");
 
-	let chats_db = server.db("amongmeme");
-
-	chats_db.collection("chats").find({});
-
+	chats_db = server.db("amongmeme");
 });
+	
+function sendChats (response)
+{	
+	let cursor = chats_db.collection("chats").find({});
+	
+	let data = [];
 
+	cursor.each( (err, doc) => {
+			if (err) {
+				console.log("Error al leer el documento");
+				throw err;
+			}
+			data.push(doc);
+		//	console.log(data);
+		
+		// if (ultimo caso){
+		//  response.write("<p>Ahora viene el chat</p>");
+		//  response.end();
+		//	}
+
+	});
+	console.log(data);
+};
 
 
 console.log("Inicializando servidor chat");
 
 
-/*
 let public_files = new node_static.Server("pub");
 
 http.createServer( (request, response) => {
 
-	console.log("Archivo"+request.url);
+	console.log("Archivo: "+request.url);
+	
+	if (request.url == "/chats"){
+		console.log("Nos piden el chat de mongo");
+		response.writeHead(200, {'Content-Type':'text/plain'});
+	//	response.write("<p>Ahora viene el chat</p>");
+	//	response.end();
 
-	public_files.serve(request, response);
+		sendChats(response);
+	} else {
+		public_files.serve(request, response);
+	}
 }).listen(8080);
-*/
+
